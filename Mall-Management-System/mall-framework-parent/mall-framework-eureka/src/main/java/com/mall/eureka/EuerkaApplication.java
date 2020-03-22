3078d57874c3831905959703502902682e444afd
ainimini <houxinjiemail@163.com>
2020-03-22 16:12:46 +0800

    [fix] moudle name error (euerka service)

--- Mall-Management-System/mall-framework-parent/mall-framework-eureka/src/main/java/com/mall/eureka/EuerkaApplication.java
+++ Mall-Management-System/mall-framework-parent/mall-framework-eureka/src/main/java/com/mall/eureka/EuerkaApplication.java
@@ -7,15 +7,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * @author X
  * @version 1.0
- * @ClassName EurekaApplication
+ * @ClassName EuerkaApplication
  * @description 注册中心启动类
- * @date 2020/3/20
+ * @date 2020/3/22
  **/
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EuerkaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class);
+        SpringApplication.run(EuerkaApplication.class);
     }
 }
